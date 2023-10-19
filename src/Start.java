480497f16f83fb45b196144b569f59d3082a91f8
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-19 09:51:46 +0200

    Eingabe eines Textes und Verknüpfung des Textes mit der Ausgabe.

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Hallo, was geht!?");
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Wie ist Ihr Name: ");
+        String name = eingabe.nextLine();
+        System.out.println("Hallo " + name + ", was geht!?");
     }
 }
